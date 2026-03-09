9fc33e077aefd591900e2a40bf6ebdee25ecf243
avergarat <avergarat@gmail.com>
2026-03-09 11:44:49 -0300

    fix tkinter PF_CASA v2

--- PF_CASA_V1_mejorado_v18_6_PF_MES_fix_CC_v20patch.py
+++ PF_CASA_V1_mejorado_v18_6_PF_MES_fix_CC_v20patch.py
@@ -1,15 +1,6 @@
-try:
-    import tkinter as tk
-except Exception:
-    tk = None
-try:
-    from tkinter import filedialog, messagebox
-except Exception:
-    filedialog = messagebox = None
-try:
-    from tkinter import ttk
-except Exception:
-    ttk = None
+import tkinter as tk
+from tkinter import filedialog, messagebox
+from tkinter import ttk
 import threading
 import re
 import zipfile
